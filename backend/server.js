623e5a1ed99f98bcfb3cb3f8d5b3af69a5cbef3e
arifmunir77 <129254701+arifmunir77@users.noreply.github.com>
2023-09-27 12:24:37 +0500

    user api created

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import connectDb from "./config/db.js";
+import path from "path";
+import userRoute from "./router/userRoute.js";
+
+import { errorHandeler, notFound } from "./middleware/errorHandler.js";
+import cors from "cors";
+import dotenv from "dotenv";
+const app = express();
+app.use(cors());
+
+dotenv.config();
+connectDb();
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is Running", process.env.PORT);
+});
+
+app.use(express.json());
+
+// parse request data content type application/json
+
+app.use("/api/user", userRoute);
+
+
+app.use(errorHandeler);
+app.use(notFound);
---
+++ frontend/src/pages/App/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import NewOrderPage from "../NewOrderPage/NewOrderPage.jsx"
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage.jsx"
+import AuthPage from "../AuthPage/AuthPage.jsx"
+import NavBar from '../../components/NavBar/NavBar.jsx'
+import { getUser } from '../../utilities/users-service';
+
+function App() {
+  const [user, setUser] = useState(getUser())
+
+  return (
+   
+      <main className="App">
+        {
+          user ?
+          <>
+          {/* NavBar and Routes are only available when user is logged in */}
+            <NavBar user={user} setUser={setUser} />
+          <Routes>
+            <Route path="/orders/new" element={<NewOrderPage />}/>
+            <Route path="/orders" element={<OrderHistoryPage />}/>
+          </Routes>
+           </>
+          :
+            <AuthPage setUser={setUser} />
+        }
+      </main>
+      
+    
+  )
+}
+
+export default App
