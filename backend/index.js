fe87fd98e09362b6196d03d4187168d1298bda0b
arifmunir77 <129254701+arifmunir77@users.noreply.github.com>
2023-09-29 22:09:22 +0500

    added backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// routes
+import AuthRoute from "./routes/AuthRoute.js";
+import UserRoute from "./routes/UserRoute.js";
+import PostRoute from "./routes/PostRoute.js";
+import UploadRoute from "./routes/UploadRoute.js";
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// to serve images inside public folder
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const CONNECTION =
+  "mongodb://test:test123@cluster0-shard-00-00.1cezz.mongodb.net:27017,cluster0-shard-00-01.1cezz.mongodb.net:27017,cluster0-shard-00-02.1cezz.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yvux9g-shard-0&authSource=admin&retryWrites=true&w=majority";
+mongoose
+  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+app.use("/auth", AuthRoute);
+app.use("/user", UserRoute);
+app.use("/posts", PostRoute);
+app.use("/upload", UploadRoute);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/App/App.jsx";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import store from "./store/ReduxStore";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// stack overflow
+
+ReactDOM.render(
+  <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
