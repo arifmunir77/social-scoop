fb8118c1dba54cfd7084efcedfc5786637d1eb89
arifmunir77 <129254701+arifmunir77@users.noreply.github.com>
2024-02-13 17:55:40 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
 
-dotenv.config();
+dotenv.config(); 
 const PORT = process.env.PORT || 5000;
 
 const CONNECTION =
